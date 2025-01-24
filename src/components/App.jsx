378b47816301a2b7961686762c71def847be81de
Abinaya-123552 <abinayarajakumar24@gmail.com>
2025-01-24 14:43:05 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar imges="https://tse1.mm.bing.net/th?id=OIP.WScT9Cv6Z-NxH9-TiT7-NgHaE8&pid=Api&rs=1&c=1&qlt=95&w=154&h=102" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Preetha",
+    imgURL:
+      "https://tse1.mm.bing.net/th?id=OIP.CuONwivmGgnU8hc-q14KkAHaEo&pid=Api&rs=1&c=1&qlt=95&w=164&h=102",
+    phone: "+123 456 789",
+    email: "b@preetha.com",
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com",
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
+];
+
+export default contacts;
